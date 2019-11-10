10213269e2b491935e60b7d23703c6f335998a0e
Todd Schneider <todd.w.schneider@gmail.com>
2019-11-10 16:56:59 -0500

    set referer header

--- players_data.R
+++ players_data.R
@@ -7,6 +7,7 @@ request_headers = c(
   "connection" = "keep-alive",
   "host" = "stats.nba.com",
   "pragma" = "no-cache",
+  "referer" = "https://www.nba.com/",
   "upgrade-insecure-requests" = "1",
   "user-agent" = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9"
 )
